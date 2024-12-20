f1d59e9e872b787eaaa0f734c63c26ea2740cbf7
Tushar Rana <tusharrana2505@gmail.com>
2024-12-20 11:56:36 +0530

    utils code discussed , api response , error , async handler  setup cors and other necesarry pakcages for the data accesed by backend

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 import express from "express";
+import cors  from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+//cors  setup
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//setup to accept any type of data coming to the backend
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true , limit:"16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+
+
 
 export {app};
