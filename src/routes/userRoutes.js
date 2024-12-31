86b785342e961a41d3f0a2d07d4ee432a4bb80d5
Tushar Rana <tusharrana2505@gmail.com>
2024-12-31 21:06:50 +0530

    adderd error handling

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,11 +7,10 @@ import {
   changecurrentpassword,
   getCurrentuser,
   updateAccountDetails,
-updateavatar,
-updatecoverimage,
-getUserchannelProfile,
-getuserwatchhistory
-
+  updateavatar,
+  updatecoverimage,
+  getUserchannelProfile,
+  getuserwatchhistory,
 } from "../controllers/userController.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "./../middlewares/auth.js";
@@ -34,22 +33,26 @@ router.route("/logout").post(verifyJWT, logoutuser);
 
 router.route("/refresh-token").post(refresaccesshtoken);
 
-
 //routes for updating user details
 
-router.route("/change-password").post(verifyJWT , changecurrentpassword);
+router.route("/change-password").post(verifyJWT, changecurrentpassword);
 
-router.route("/current-user").get(verifyJWT,getCurrentuser);
+router.route("/current-user").get(verifyJWT, getCurrentuser);
 
-router.route("/update-account-details").patch(verifyJWT,updateAccountDetails);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
 
-router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateavatar);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateavatar);
 
-router.route("/update-cover-image").patch(verifyJWT,upload.single("/coverImage"),updatecoverimage);
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updatecoverimage);
 
+router
+  .route("/channel-profile/:username")
+  .get(verifyJWT, getUserchannelProfile);
 
-router.route("/channel-profile/:username").get(verifyJWT,getUserchannelProfile);
-
-router.route("/watch-history").get(verifyJWT,getuserwatchhistory);
+router.route("/watch-history").get(verifyJWT, getuserwatchhistory);
 
 export default router;
